570fa8f096bccb7d5e5fd83c863e8449e19815da
contato <allanbatista@Allans-MacBook-Pro.local>
2024-09-01 11:37:21 -0300

    init

---
+++ src/app/imersao-deip/components/HeaderImersao.tsx
@@ -0,0 +1,11 @@
+const HeaderImersao = () => {
+    return (
+        <section className="bg-imersao h-screen  md:h-[400px]">
+            <div className="container">
+
+            </div>
+        </section>
+    )
+}
+
+export default HeaderImersao;
---
+++ src/app/imersao-deip/page.tsx
@@ -0,0 +1,9 @@
+import HeaderImersao from "./components/HeaderImersao";
+
+export default function ImercaoDeip() {
+    return (
+        <div>
+            <HeaderImersao />
+        </div>
+    )
+}
